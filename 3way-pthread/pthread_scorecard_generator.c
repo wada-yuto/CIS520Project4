ddc9117a0bff3d634be4f795885167463370f5e5
Micah Roening <roening@eos.beocat.ksu.edu>
2023-04-16 15:34:50 -0500

    pthread file changes

---
+++ 3way-pthread/pthread_scorecard_generator.c
@@ -0,0 +1,27 @@
+/**
+ * Author: Micah Roening
+ * Last updated: 4/14/23
+ *
+ * Outputs the maximum ASCII character numeric value from every line
+ * in a specified .txt file in order.
+ *
+ * TODO: implement main
+ *       discover how to find core count
+ *       implement int get_file()
+ *       implement int count_lines()
+ *       implement void* generate_scoreboard_section();
+ *       implement void print_scoreboard();
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
+
+// serial count-line function goes here
+
+// threaded calculate, store, and return scorecard information goes here
