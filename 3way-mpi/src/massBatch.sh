43dbc766d9fd6e16dc06ad2a5b26db60f01aa6ac
wada-yuto <67665277+wada-yuto@users.noreply.github.com>
2023-04-30 22:49:51 -0500

    Delete massBatch.sh
    Delete massBatch.sh

--- 3way-mpi/src/massBatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-##$ -l h_rt=0:01:00		# ask for 1 hour runtime
-#SBATCH --constraint=moles
-for i in 1 2
-do
-	 sbatch --constraint=mole --ntasks-per-node=$i --nodes=1 --mem-per-cpu=$((16/$i))G smallBatch.sh
-done
